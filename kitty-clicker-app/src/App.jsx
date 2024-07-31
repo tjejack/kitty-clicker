c285957d117ababebbfecde5ad0cf56006ded7c1
tjejack <tjeidanjack@gmail.com>
2024-07-31 17:58:17 +0100

    logic for pageview and importing components

--- kitty-clicker-app/src/App.jsx
+++ kitty-clicker-app/src/App.jsx
@@ -1,11 +1,32 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import NavBar from "./Components/NavBar";
+import TrackerBar from "./Components/TrackerBar";
+import AssetsPage from "./Components/AssetsPage";
+import ShopPage from "./Components/ShopPage";
+import TrophiesPage from "./Components/TrophiesPage";
+import HomePage from "./Components/HomePage";
 
 function App() {
+  const [pageView, setPageView] = useState("Home");
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Kitty Clicker</h1>
-      </header>
+      <Header />
+      <TrackerBar />
+      {(() => {
+        switch (pageView) {
+          case "Assets":
+            return <AssetsPage />;
+          case "Shop":
+            return <ShopPage />;
+          case "Trophies":
+            return <TrophiesPage />;
+          default:
+            return <HomePage />;
+        }
+      })()}
+      <NavBar setPageView={setPageView} />
     </div>
   );
 }
