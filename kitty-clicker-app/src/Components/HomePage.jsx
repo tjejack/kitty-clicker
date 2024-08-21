181f56c270d96a88cad8d627c1d3baf42f4df256
tjejack <tjeidanjack@gmail.com>
2024-08-21 17:58:08 +0100

    added cat head clickable button with state change

--- kitty-clicker-app/src/Components/HomePage.jsx
+++ kitty-clicker-app/src/Components/HomePage.jsx
@@ -1,9 +1,23 @@
+import catStandard from "../kitty-clicker-assets/cat-head-standard.png";
+import catHappy from "../kitty-clicker-assets/cat-head-happy.png"
+import { useState } from "react";
+
 function HomePage() {
-    return(
-        <div>
-        <p>This is the Home Page</p>
-      </div>
-    )
+  const [catState, setCatState] = useState(catStandard);
+  const handlePress = () => {
+    setCatState(catHappy)
+  }
+  const handleRelease = () => {
+    setCatState(catStandard)
+  }
+  return (
+    <div>
+      <p>This is the Home Page</p>
+      <button onPointerDown={handlePress} onKeyDown={handlePress} onPointerUp={handleRelease} onKeyUp={handleRelease}>
+        <img src={catState} alt="Ginger cat head in pixel style" />
+      </button>
+    </div>
+  );
 }
 
 export default HomePage;
